4b576f226d118987c069177a5a819a3a857d74ff
DrBrawn <drbrawn@yandex.ru>
2023-02-27 15:21:44 +0300

    Complete task3

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+bool Validate(int num)
+{
+    if (num >= 100 && num < 1000)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Это число не трехзначное");
+    return false;
+}
+
+int number = Prompt("Введите трехзначное число");
+
+if (Validate(number))
+{
+    int secondDigit = number / 10 % 10;
+    System.Console.WriteLine($"Вторая цифра числа {number} равна {secondDigit}");
+}
