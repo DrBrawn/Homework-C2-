d7f8fc6027b55a2a0aa46cb54d16a646c575be64
DrBrawn <drbrawn@yandex.ru>
2023-02-27 16:06:16 +0300

    Complete task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+bool Validate(int num)
+{
+    if (num > 0 && num <= 7)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Это не день недели.");
+    return false;
+}
+
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+int weekDay = Prompt("Введите день недели");
+if (Validate(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        System.Console.WriteLine("Выходной!");
+    }
+    else
+    {
+        System.Console.WriteLine("На работу.");  
+    } 
+}
